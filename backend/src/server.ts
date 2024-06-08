10264c5ecbd729816c9dc07b639c3a4caf6a1433
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-08 19:59:37 -0300

    feat: initial setup

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import { app } from './app'
+import { createTables } from './database/create-tables'
+
+app.listen({
+  host: '0.0.0.0',
+  port: 3333
+})
+  .then(() => {
+    console.log('Server is running at http://localhost:3333')
+    createTables()
+  })
+  .catch((error) => {
+    console.log('Error starting server: ', error.message)
+  })
