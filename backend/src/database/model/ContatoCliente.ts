4eb77312f94a44a7cf7e09c55f9ea6b1a1b4a435
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-16 18:25:14 -0300

    feat: Add models for ContatoCliente and EnderecoCliente
    The commit adds new models for `ContatoCliente` and `EnderecoCliente` to the backend database. These models define the structure and properties of the respective entities. The `ContatoCliente` model includes properties such as `id`, `id_cliente`, `info`, and `tipo`. Similarly, the `EnderecoCliente` model includes properties such as `id`, `id_cliente`, `nome`, `bairro`, `rua`, `cep`, `cidade`, and `number`.
    
    Co-authored-by: Thiago Trainotti <thisgot@users.noreply.github.com>

---
+++ backend/src/database/model/ContatoCliente.ts
@@ -0,0 +1,6 @@
+export type ContatoFornecedor = {
+  id: number,
+  id_cliente: number,
+  info: string,
+  tipo: string,
+}
---
+++ backend/src/database/model/EnderecoCliente.ts
@@ -0,0 +1,10 @@
+export type EnderecoCliente = {
+  id: number,
+  id_cliente: number,
+  nome: string,
+  bairro: string,
+  rua: string,
+  cep: string,
+  cidade: string,
+  number: string
+}
