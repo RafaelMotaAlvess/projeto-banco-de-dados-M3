79112d485568ab502a164c651b411f8a584d617c
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-16 17:15:43 -0300

    feat: Add models for DetalhesDoPedido, Pedido, and Status
    Co-authored-by: Thiago Trainotti <thisgot@users.noreply.github.com>

---
+++ backend/src/database/model/Pedido.ts
@@ -0,0 +1,7 @@
+export type Pedido = {
+  id: number,
+  id_cliente: number,
+  data: Date,
+  metodo_compra: string,
+  total_pedido: number,
+}
---
+++ backend/src/database/model/Status.ts
@@ -0,0 +1,8 @@
+export type Status = {
+  id: number,
+  id_pedido: number,
+  descricao: string,
+  observacoes: string,
+  total_pedido: number,
+  data_atualizacao?: Date
+}
