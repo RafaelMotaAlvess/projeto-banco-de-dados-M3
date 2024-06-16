06a57df04b72b38691e578138ee94ef68f90112d
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-16 18:18:34 -0300

    feat: Add models for DetalhesDoPedido, Pedido, and Produto
    The commit adds new models for `DetalhesDoPedido`, `Pedido`, and `Produto` to the backend database. These models define the structure and properties of the respective entities. The `Pedido` model now includes a new property `detalhes` which represents the details of the order. Additionally, the `Produto` model is a new file that defines the properties of a product.
    
    Co-authored-by: Thiago Trainotti <thisgot@users.noreply.github.com>

--- backend/src/database/model/Pedido.ts
+++ backend/src/database/model/Pedido.ts
@@ -1,7 +1,10 @@
+import { DetalhesDoPedido } from "./DetalhesDoPedido"
+
 export type Pedido = {
   id: number,
   id_cliente: number,
   data: Date,
   metodo_compra: string,
   total_pedido: number,
+  detalhes: DetalhesDoPedido[]
 }
---
+++ backend/src/database/model/Produto.ts
@@ -0,0 +1,9 @@
+export type Produto = {
+  id: number,
+  id_categoria: number,
+  id_fornecedor: number,
+  nome: string,
+  descricao: string,
+  preco: number,
+  quantidade_estoque: number
+}
