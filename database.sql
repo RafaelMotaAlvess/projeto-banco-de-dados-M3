d4f51e0ce935f23f818457b9931a235a99f8fd8e
RafaelMotaAlvess <leafardzn@gmail.com>
2024-06-16 23:32:25 -0300

    feat: add database schema and tables
    Co-authored-by: Thiago Trainotti <thisgot@users.noreply.github.com>

---
+++ database.sql
@@ -0,0 +1,101 @@
+CREATE SCHEMA `projetodbm3`;
+USE projetodbm3;
+
+CREATE TABLE IF NOT EXISTS Fornecedor (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS ContatoFornecedor (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  info VARCHAR(30),
+  tipo VARCHAR(20),
+  id_fornecedor INT,
+  FOREIGN KEY (id_fornecedor) REFERENCES Fornecedor (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS EnderecoFornecedor (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  bairro VARCHAR(50),
+  rua VARCHAR(50),
+  cep VARCHAR(8),
+  cidade VARCHAR(50),
+  estado VARCHAR(50),
+  numero VARCHAR(6),
+  id_fornecedor INT,
+  FOREIGN KEY (id_fornecedor) REFERENCES Fornecedor (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Cliente (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50),
+  email VARCHAR(50),
+  senha VARCHAR(50),
+  data_criacao DATE,
+  data_atualizacao DATE
+);
+
+CREATE TABLE IF NOT EXISTS ContatoCliente (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  info VARCHAR(30),
+  tipo VARCHAR(20),
+  id_cliente INT,
+  FOREIGN KEY (id_cliente) REFERENCES Cliente (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS EnderecoCliente (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  bairro VARCHAR(50),
+  rua VARCHAR(50),
+  cep INT,
+  cidade VARCHAR(50),
+  estado VARCHAR(50),
+  numero INT,
+  id_cliente INT,
+  FOREIGN KEY (id_cliente) REFERENCES Cliente (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Categoria (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50)
+);
+
+CREATE TABLE IF NOT EXISTS Produto (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  nome VARCHAR(50),
+  descricao VARCHAR(500),
+  preco FLOAT(12),
+  quantidade_estoque INT,
+  id_categoria INT,
+  id_fornecedor INT,
+  FOREIGN KEY (id_fornecedor) REFERENCES Fornecedor (id) ON DELETE CASCADE,
+  FOREIGN KEY (id_categoria) REFERENCES Categoria (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Pedido (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  data DATE,
+  metodo_compra VARCHAR(20),
+  total_pedido FLOAT(12),
+  id_cliente INT,
+  FOREIGN KEY (id_cliente) REFERENCES Cliente (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Detalhes (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  quantidade_produto INT,
+  subtotal FLOAT,
+  id_pedido INT,
+  id_produto INT,
+  FOREIGN KEY (id_pedido) REFERENCES Pedido (id) ON DELETE CASCADE,
+  FOREIGN KEY (id_produto) REFERENCES Produto (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS Status (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  descricao VARCHAR(255),
+  data_atualizacao DATE,
+  observacoes VARCHAR(100),
+  id_pedido INT,
+  FOREIGN KEY (id_pedido) REFERENCES Pedido (id) ON DELETE CASCADE
+);
